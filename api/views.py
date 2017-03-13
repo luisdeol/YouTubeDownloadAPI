b80cb7d28fce2a329ba63c3be2070e2e4b8a0f2a
luisfelipekai <luisfelipekai@gmail.com>
2017-03-13 16:20:00 -0300

    Implement view method using url passed as query string.

--- api/views.py
+++ api/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from winreg import *
 from bs4 import BeautifulSoup
 import urllib.request
 from pytube import YouTube
-
+import os
 
 with OpenKey(HKEY_CURRENT_USER, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders') as key:
     path = QueryValueEx(key, '{374DE290-123F-4565-9164-39C4925E467B}')[0] + '\\videos\\'
@@ -14,8 +13,9 @@ with OpenKey(HKEY_CURRENT_USER, 'SOFTWARE\Microsoft\Windows\CurrentVersion\Explo
 # Create your views here.
 @api_view(['GET'])
 def download_videos(request):
-    # url = request.GET.get('url')
-    url = "https://www.youtube.com/channel/UCLLlKJZVbeUTzwjVM7Lx9kg"
+    url = request.GET.get('url')
+    if not os.path.isdir(path):
+        os.makedirs(path)
     html = urllib.request.urlopen(url).read()
     soup = BeautifulSoup(html, "html.parser")
     h3s = soup.findAll("h3", class_="yt-lockup-title")
